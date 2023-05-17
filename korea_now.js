91a9305326fb7ac088a3b9ef700e87e21a182179
gaurang-workshore <122290554+gaurang-workshore@users.noreply.github.com>
2023-05-17 19:21:00 +0530

    Create korea_now.js

---
+++ korea_now.js
@@ -0,0 +1,10 @@
+var swiper = new Swiper('.home-slider-wrap', {
+    slidesPerView: 1,
+    spaceBetween: 30,
+    grabCursor: true,
+    speed: 500,
+    pagination: {
+      el: '.slider__pagination',
+      clickable: true,
+    },
+});
